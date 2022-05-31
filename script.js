67fc55de775cc4426faeb1cdaa65b661d895d603
fracabu <100199965+fracabu@users.noreply.github.com>
2022-05-31 16:24:52 +0200

    timer 30 secondi

---
+++ script.js
@@ -0,0 +1,31 @@
+/*
+Visualizzare in pagina (html) 5 numeri casuali.
+Avviare un timer di 30 secondi
+Dopo 30 secondi, nascondere i numeri.
+L’utente deve inserire, uno alla volta, i numeri che ha visto 
+precedentemente, tramite un prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti 
+e quali dei numeri da indovinare sono stati individuati.
+*/
+
+let generateRandomNumber = Math.floor(Math.random() * 6);
+console.log(generateRandomNumber);
+let secondiRimanenti = 30;
+
+// Genero cinque numeri casuali nella pagina html
+for (let i = 0; i<5; i++){
+    generateRandomNumber = document.createElement("container");
+    generateRandomNumber.innerText = (Math.floor(Math.random() * 6));
+    document.body.append(generateRandomNumber);
+}
+
+// Timer 30 secondi
+const timerSecondi = setInterval (function(){
+    console.log(secondiRimanenti);
+
+secondiRimanenti--;
+}, 1000);
+
+setTimeout(function(){
+    clearInterval(timerSecondi);
+},1000*31);
